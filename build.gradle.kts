52859a94cfe75309efec35f28fc63cff3d531bc5
shinyay <shinya.com@gmail.com>
2020-03-14 16:52:54 +0900

    Add Swagger configuration fragment

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("io.micrometer:micrometer-registry-prometheus:latest.release")
+	implementation("io.springfox:springfox-swagger2:2.9.2")
+	implementation("io.springfox:springfox-swagger-ui:2.9.2")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
---
+++ src/main/kotlin/io/pivotal/shinyay/config/SwaggerConfig.kt
@@ -0,0 +1,9 @@
+package io.pivotal.shinyay.config
+
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+}
