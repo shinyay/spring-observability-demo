b4675cbc251de5f6cc363be79b784aaa74a01589
shinyay <shinya.com@gmail.com>
2020-03-14 16:38:53 +0900

    Add a functions to map repository functions

--- src/main/kotlin/io/pivotal/shinyay/controller/PersonController.kt
+++ src/main/kotlin/io/pivotal/shinyay/controller/PersonController.kt
@@ -1,9 +1,25 @@
 package io.pivotal.shinyay.controller
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import io.pivotal.shinyay.entity.Person
+import io.pivotal.shinyay.repository.PersonRepository
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/persons")
-class PersonController {
+class PersonController(var repository: PersonRepository) {
+
+    @GetMapping("/")
+    fun findAll() = repository.findAll()
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Int) = repository.findById(id)
+
+    @PostMapping("/")
+    fun add(@RequestBody person: Person) = repository.save(person)
+
+    @PutMapping("/")
+    fun update(@RequestBody person: Person) = repository.update(person)
+
+    @DeleteMapping("{id}")
+    fun delete(@PathVariable id: Int) = repository.removeById(id)
 }
