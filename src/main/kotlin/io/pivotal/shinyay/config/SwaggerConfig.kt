adbc563c8e63f8ddcdae934e22df3b6c332310de
shinyay <shinya.com@gmail.com>
2020-03-17 22:50:34 +0900

    Add Swagger API Documentation configuration function

--- src/main/kotlin/io/pivotal/shinyay/config/SwaggerConfig.kt
+++ src/main/kotlin/io/pivotal/shinyay/config/SwaggerConfig.kt
@@ -3,14 +3,29 @@ package io.pivotal.shinyay.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.service.ApiInfo
 import springfox.documentation.service.Contact
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger.web.*
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
 @EnableSwagger2
 class SwaggerConfig {
+
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths { it.equals("/persons") }
+                .build()
+                .useDefaultResponseMessages(false)
+                .forCodeGeneration(true)
+    }
 
     @Bean
     fun uiConfig(): UiConfiguration {
